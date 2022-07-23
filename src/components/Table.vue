6e5540beed67a9c6c9751da856e3ac335bd3499b
Stephen Allen <allens1015@gmail.com>
2022-07-23 19:54:00 -0400

    Removed headers, mid-data-upgrade

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,14 +1,6 @@
 <template>
   <v-container>
     <h3 class="stat-table-header">{{ localMaps.monsterStatsMap[tableTypeIn] }}</h3>
-    <!-- <v-row>
-      <v-col
-        v-for="(header,i) in getTableHeaders(tableTypeIn)"
-        :key="i"
-      >
-        {{ header }}
-      </v-col>
-    </v-row> -->
     <v-row
       v-for="(row,i) in getTableData(tableTypeIn)"
       :key="i"
@@ -24,11 +16,6 @@
       </v-col>
     </v-row>
     <v-divider></v-divider>
-
-<!-- {{ getTableData(tableTypeIn) }} -->
-    <!-- {{ headers }}
-    {{ getTableData() }}
-    {{ tableType }} -->
   </v-container>
 </template>
 
@@ -44,32 +31,10 @@ export default ({
     monsterRoles,
     localMaps,
     tableType: "stats",
-    headers: ["Extreme","High","Moderate","Low","Terrible"],
-    defaultHeaders: ["Extreme","High","Moderate","Low","Terrible"],
     tableData: [],
     isFocusedArray: []
   }),
   methods: {
-    getTableHeaders(prop) {
-      const tableData = monsterRoles[prop];
-      if(tableData[0].length == 4) {
-        return ["Extreme","High","Moderate","Low"]
-      }
-      if(prop == "spellAttack" || prop == "spellDC") {
-        if(tableData[0].length == 3) {
-          return ["Extreme","High","Moderate"]
-        }
-      }
-      else {
-        if(tableData[0].length == 3) {
-          return ["High","Moderate","Low"]
-        }
-      }
-      if(tableData[0].length == 2) {
-        return ["Maximum","Minimum"]
-      }
-      return this.defaultHeaders;
-    },
     getTableData(prop) {
       return monsterRoles[prop];
     },
