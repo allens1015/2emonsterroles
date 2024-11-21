ede68d424985e59a5dc0c2693b3aae7dbaf130f7
Stephen Allen <allens1015@gmail.com>
2024-11-21 18:32:11 -0500

    More relative linking?

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === "production" ? "/2emonsterroles/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/2emonsterroles/dist/" : "/"
 }
